497ec9484c7f1be72989b132f5a366f10d8f80f5
Pascal Cadotte Michaud <pcm@wazo.io>
2018-02-01 11:26:46 -0500

    remove API 0.1 compatibility

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2018 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
 from setuptools import setup
@@ -8,7 +8,7 @@ from setuptools import find_packages
 
 setup(
     name='wazo_plugind_client',
-    version='0.1',
+    version='0.2',
 
     description='a simple client library for the wazo-plugind HTTP interface',
 

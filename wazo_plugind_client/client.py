b77d06a2dd56d7ecba1fad7890d96424b07da9a5
Francois Blackburn <fblackburn@wazo.community>
2019-10-13 16:08:51 -0400

    use full name for entry points

--- wazo_plugind_client/client.py
+++ wazo_plugind_client/client.py
@@ -7,7 +7,7 @@ from wazo_lib_rest_client.client import BaseClient
 
 class PlugindClient(BaseClient):
 
-    namespace = 'plugind_client.commands'
+    namespace = 'wazo_plugind_client.commands'
 
     def __init__(self, host, port=9503, version='0.2', **kwargs):
         super(PlugindClient, self).__init__(host=host, port=port, version=version, **kwargs)

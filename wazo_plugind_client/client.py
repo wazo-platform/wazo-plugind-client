d55a779b0e1a9f81ebc2073e8c82e5f41a1fd933
Jesse Sopel <jesse.sopel@wazo.io>
2022-12-21 09:55:32 -0500

    Remove Python2 build and deps

--- wazo_plugind_client/client.py
+++ wazo_plugind_client/client.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017-2020 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_lib_rest_client.client import BaseClient
@@ -10,6 +9,4 @@ class PlugindClient(BaseClient):
     namespace = 'wazo_plugind_client.commands'
 
     def __init__(self, host, port=443, prefix='/api/plugind', version='0.2', **kwargs):
-        super(PlugindClient, self).__init__(
-            host=host, port=port, prefix=prefix, version=version, **kwargs
-        )
+        super().__init__(host=host, port=port, prefix=prefix, version=version, **kwargs)
